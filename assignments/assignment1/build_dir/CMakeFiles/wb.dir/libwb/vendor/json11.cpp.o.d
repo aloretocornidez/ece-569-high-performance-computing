6d608b5ffa66632a9fddf29153f2a479c6b8c711
Alan Manuel Loreto Cornídez <aloretocornidez@arizona.edu>
2023-02-02 00:28:36 -0700

    i can do stuff on my pc now

---
+++ assignments/assignment1/build_dir/CMakeFiles/wb.dir/libwb/vendor/json11.cpp.o.d
@@ -0,0 +1,167 @@
+CMakeFiles/wb.dir/libwb/vendor/json11.cpp.o: \
+ /home/alan-manuel/Downloads/school/569-high-performance-computing/assignments/assignment1/labs/libwb/vendor/json11.cpp \
+ /usr/include/stdc-predef.h \
+ /home/alan-manuel/Downloads/school/569-high-performance-computing/assignments/assignment1/labs/libwb/vendor/json11.hpp \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/string \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/x86_64-pc-linux-gnu/bits/c++config.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/x86_64-pc-linux-gnu/bits/cpu_defines.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stringfwd.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/memoryfwd.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/char_traits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_algobase.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/functexcept.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/exception_defines.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/cpp_type_traits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/ext/type_traits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/ext/numeric_traits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_pair.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/move.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/type_traits \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_iterator_base_types.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_iterator_base_funcs.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/concept_check.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/debug/assertions.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_iterator.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/ptr_traits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/debug/debug.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/predefined_ops.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/postypes.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/cwchar \
+ /usr/include/wchar.h /usr/include/bits/libc-header-start.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/stddef.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/stdarg.h \
+ /usr/include/bits/wchar.h /usr/include/bits/types/wint_t.h \
+ /usr/include/bits/types/mbstate_t.h \
+ /usr/include/bits/types/__mbstate_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/locale_t.h \
+ /usr/include/bits/types/__locale_t.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/cstdint \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/allocator.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/x86_64-pc-linux-gnu/bits/c++allocator.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/ext/new_allocator.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/new \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/exception.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/localefwd.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/x86_64-pc-linux-gnu/bits/c++locale.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/clocale \
+ /usr/include/locale.h /usr/include/bits/locale.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/iosfwd \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/cctype \
+ /usr/include/ctype.h /usr/include/bits/endian.h \
+ /usr/include/bits/endianness.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/ostream_insert.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/cxxabi_forced.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_function.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/backward/binders.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/range_access.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/initializer_list \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/basic_string.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/ext/atomicity.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/x86_64-pc-linux-gnu/bits/gthr.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/x86_64-pc-linux-gnu/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/struct_timespec.h /usr/include/bits/sched.h \
+ /usr/include/bits/types/struct_sched_param.h /usr/include/bits/cpu-set.h \
+ /usr/include/time.h /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/clock_t.h /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/clockid_t.h /usr/include/bits/types/timer_t.h \
+ /usr/include/bits/types/struct_itimerspec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h \
+ /usr/include/bits/atomic_wide_counter.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/bits/setjmp.h \
+ /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct___jmp_buf_tag.h \
+ /usr/include/bits/pthread_stack_min-dynamic.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/x86_64-pc-linux-gnu/bits/atomic_word.h \
+ /usr/include/sys/single_threaded.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/ext/alloc_traits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/alloc_traits.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_construct.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/ext/string_conversions.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/cstdlib \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/sys/types.h \
+ /usr/include/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
+ /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
+ /usr/include/alloca.h /usr/include/bits/stdlib-bsearch.h \
+ /usr/include/bits/stdlib-float.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/std_abs.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/cstdio \
+ /usr/include/stdio.h /usr/include/bits/types/__fpos_t.h \
+ /usr/include/bits/types/__fpos64_t.h \
+ /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/stdio.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/cerrno \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/bits/types/error_t.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/charconv.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/functional_hash.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/hash_bytes.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/basic_string.tcc \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/vector \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_uninitialized.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_vector.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_bvector.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/vector.tcc \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/map \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_tree.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/ext/aligned_buffer.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_map.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/tuple \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/utility \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_relops.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/array \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/uses_allocator.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/invoke.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_multimap.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/erase_if.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/memory \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_tempbuf.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/stl_raw_storage_iter.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/align.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bit \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/unique_ptr.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/shared_ptr.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/shared_ptr_base.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/typeinfo \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/allocated_ptr.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/refwrap.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/ext/concurrence.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/exception \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/exception_ptr.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/cxxabi_init_exception.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/nested_exception.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/shared_ptr_atomic.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/atomic_base.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/bits/atomic_lockfree_defines.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/backward/auto_ptr.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/cinttypes \
+ /usr/include/inttypes.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/cassert \
+ /usr/include/assert.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/cmath \
+ /usr/include/math.h /usr/include/bits/math-vector.h \
+ /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
+ /usr/include/bits/iscanonical.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/11.3.0/include/c++/limits
---
+++ assignments/assignment1/build_dir/CMakeFiles/wb.dir/libwb/wbMPI.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/wb.dir/libwb/wbMPI.cpp.o: \
+ /home/alan-manuel/Downloads/school/569-high-performance-computing/assignments/assignment1/labs/libwb/wbMPI.cpp \
+ /usr/include/stdc-predef.h
