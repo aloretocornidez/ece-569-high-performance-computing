d273ea499de25823acd80e0140366489d6f81898
Alan Manuel Loreto Cornídez <aloretocornidez@arizona.edu>
2023-03-03 09:31:36 -0700

    fir git ignore mess

--- homework/assignment-container/build_dir/CMakeFiles/wb.dir/libwb/wbMPI.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/wb.dir/libwb/wbMPI.cpp.o: \
- /home/alan-manuel/Downloads/school/569-high-performance-computing/assignments/assignment-container/labs/libwb/wbMPI.cpp \
- /usr/include/stdc-predef.h
